1b412934d0b45a35cf9b1ae453be9f1b33cab0dd
originalboss <50785765+originalboss@users.noreply.github.com>
2024-09-23 20:36:09 -0700

    Add files via upload

--- ping-coming-soon-page-master/Ping-coming-soonpage-master/ping-coming-soon-page-master/sandbox.js
+++ ping-coming-soon-page-master/Ping-coming-soonpage-master/ping-coming-soon-page-master/sandbox.js
@@ -1,26 +1,46 @@
 let btn = document.querySelector("#sub");
 let mail = document.querySelector("input.mail");
+let realemail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
 
 
-
-function validateEmail(){
+function validateEmail(email){
 
     if( mail.value === "" || mail.value === undefined){
     
          let msg = document.querySelector("p");
          msg.innerText = "Please enter your email"; 
-         msg.style.opacity = 1;
-         msg.style.color = "red"; 
-         msg.style.position = "relative"; 
-         msg.style.textEmphasis;
-        
-
-        // msg.classList.add("error");
+        //  msg.style.opacity = 1;
+        //  msg.style.color = "red"; 
+        //  msg.style.position = "relative"; 
+        //  msg.style.textEmphasis;
+        msg.classList.add("error");
+        mail.classList.add("error");
         // mail.style.BorderColor = "red" ;    
     }
-}
+    else if(!email.value.match(realemail)){
+        let msg = document.querySelector("p");
+        msg.innerText = "Please enter a valid email"; 
+        msg.classList.add("error");
+        mail.classList.add("error"); 
+   
+    }
+
+    else{
+
+        let msg = document.querySelector("p");
+        msg.innerText = "Thanks :)";
+        msg.classList.add("success");
+        mail.classList.add("success");
+
+    }
+
+
+
+
+
+    }
 
 
 
